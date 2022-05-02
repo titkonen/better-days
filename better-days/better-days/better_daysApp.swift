95b396a9200bccb79ea6a820274c355f79b1f0ed
Toni Itkonen <toni.itkonen@icloud.com>
2022-05-02 13:07:09 +0300

    + Add: Initial Commit

---
+++ better-days/better-days/better_daysApp.swift
@@ -0,0 +1,17 @@
+//
+//  better_daysApp.swift
+//  better-days
+//
+//  Created by Toni Itkonen on 2.5.2022.
+//
+
+import SwiftUI
+
+@main
+struct better_daysApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
